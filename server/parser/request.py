e34764bac1634620496ef0abf17ae8d90cc535e2
twardy7991 <138113954+twardy7991@users.noreply.github.com>
2026-05-12 18:22:30 +0200

    small refactor of parser.py to make it more modular (#5)
    * small refactor of parser.py to make it more modular
    
    * small fix
    
    * small fix to make pylance properly recognize header/line types

---
+++ server/parser/request.py
@@ -0,0 +1,35 @@
+from server.parser.message import Header, Line, Message
+
+class RequestHeader(Header):
+    pass
+
+class RequestLine(Line):
+    def __init__(self, method, url, version):
+        super().__init__(version)
+        self.method = method
+        self.url = url 
+    
+    def __str__(self):
+        return f"{self.method} {self.url} {self.version}\r\n"
+
+    def __eq__(self, other):
+        if isinstance(other, RequestLine): 
+            if not self.method == other.method:
+                print(f"{self.method} not equal {other.method}")
+                return False
+            if not self.url == other.url:
+                print(f"{self.url} not equal {other.url}")
+                return False
+            if not self.version == other.version:
+                print(f"{self.version} not equal {other.version}")
+                return False
+            return True
+        
+        raise TypeError(f"= not supported between instances of '{self.__class__}' and '{type(other)}'")
+
+class Request(Message):
+    line : RequestLine
+    header : RequestHeader
+    
+    def __init__(self, line : RequestLine, header : RequestHeader, body : Line):
+        super().__init__(line, header, body)
---
+++ server/parser/response.py
@@ -0,0 +1,19 @@
+from server.parser.message import Header, Line, Message
+from util.consts import STATUS_CODE
+
+class ResponseHeader(Header):
+    def add_header(self, header, value):
+        self.headers[header] = value
+
+class ResponseLine(Line):
+    def __init__(self, version, status_code):
+        super().__init__(version)
+        self.status_code = status_code
+        self.phrase = STATUS_CODE[int(status_code)]
+
+class Response(Message):
+    line: ResponseLine 
+    header: ResponseHeader
+    
+    def __init__(self, line : ResponseLine, header : ResponseHeader, body : Line):
+        super().__init__(line, header, body)
