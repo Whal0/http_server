af246129e6e4df3864b53f97355c77484f99f680
twardy7991 <patryktwardowski79@gmail.com>
2026-05-16 12:51:45 +0200

    added handler with basic logic + tests

---
+++ server/response_handler.py
@@ -0,0 +1,114 @@
+from server.parser.response import Response, ResponseHeader, ResponseLine 
+import datetime
+from server.parser.request import Request
+from server.file_manager import FileManager, File
+from server.exceptions import DirectoryAccessForbiddenException, FileNotFoundException
+
+class ResponseHandler:
+
+    def __init__(self, file_manager):
+        self.file_manager : FileManager = file_manager
+
+    @staticmethod
+    def _create_response() -> Response:
+        return Response(
+            ResponseLine(
+                version="HTTP/1.1",
+                status_code=200
+            ),
+            ResponseHeader(
+                headers={}
+                #"Date" : datetime.datetime.now()
+            )
+        )
+    
+    @staticmethod
+    def create_response_400() -> Response:
+        response = __class__._create_response()        
+        response.line.status_code = 400
+
+        return response
+    
+    @staticmethod
+    def _create_response_501() -> Response:
+        response = __class__._create_response()        
+        response.line.status_code = 400
+
+        return response
+    
+    def handle_request(self, request : Request) -> Response:
+
+        match request.line.method:
+            case "GET":
+                return self._handle_get(request=request)
+
+            case "HEAD":
+                return self._handle_head(request=request) 
+            
+            case "PUT":
+                return self._handle_put(request=request)
+            
+            case "OPTIONS":
+                return self._handle_options(request=request)
+            
+            case _:
+                return self._create_response_501()
+        
+    def _handle_get(self, request : Request) -> Response:
+        response : Response = self._create_response()
+
+        try:
+            file : File = self.file_manager.get_file(request.line.url)
+
+            response.line.status_code = 200
+            response.header.add_header("Content-Type", file.mime_type)
+            response.header.add_header("Content-Length", file.size)
+            response.header.add_header("Last-Modified", file.last_modified)
+        
+            response.body = file.data
+        except PermissionError:
+            response.line.status_code = 400
+        except (DirectoryAccessForbiddenException, FileNotFoundError):
+            response.line.status_code = 404
+        
+        return response
+
+    def _handle_head(self, request : Request) -> Response:
+        response : Response = self._create_response()
+
+        try:
+            file : File = self.file_manager.get_metadata(request.line.url)
+            response.header.add_header("Content-Type", file.mime_type)
+            response.header.add_header("Content-Length", file.size)
+            response.header.add_header("Last-Modified", file.last_modified)
+
+        except PermissionError:
+            response.line.status_code = 400
+        except (DirectoryAccessForbiddenException, FileNotFoundError):
+            response.line.status_code = 404
+
+        return response
+
+    def _handle_options(self, request : Request) -> Response:
+        response : Response = self._create_response()
+        
+        response.line.status_code = 204
+        response.header.add_header("Allow", "GET, POST, OPTIONS, HEAD")
+
+        return response
+
+    def _handle_put(self, request : Request) -> Response:
+        response : Response = self._create_response()
+
+        try:
+            self.file_manager.put_file(request.line.url)
+
+            response.line.status_code = 201
+            response.header.add_header("Content-Location", request.line.url)
+
+        except OSError:
+            response.line.status_code = 500
+        except (PermissionError, DirectoryAccessForbiddenException):
+            response.line.status_code = 400
+
+        return response
---
+++ tests/test_response_handler.py
@@ -0,0 +1,136 @@
+import pytest
+from unittest.mock import MagicMock
+from server.response_handler import ResponseHandler
+from server.parser.request import Request, RequestHeader, RequestLine
+from server.parser.response import Response, ResponseHeader, ResponseLine
+
+@pytest.fixture
+def mock_file(mock_metadata):
+    file = mock_metadata
+    file.data = bytes("pykpykpyk", encoding="utf-8")
+    yield file
+
+@pytest.fixture
+def mock_metadata():
+    metadata = MagicMock()
+    metadata.path = "kulawykonfident.com/komendapłock.html"
+    metadata.mime_type = "text/html"
+    metadata.last_modified = "Fri, 15 May 2026 21:08:42 GMT"
+    metadata.data = "Fri, 15 May 2026 22:08:42 GMT"
+    metadata.size = "9"
+    metadata.is_directory = False
+    yield metadata
+
+@pytest.fixture
+def mock_file_manager(mock_file, mock_metadata):
+    file_manager = MagicMock()
+    file_manager.get_file.return_value = mock_file
+    file_manager.get_metadata.return_value = mock_metadata
+    file_manager.put_file.return_value = None
+    yield file_manager
+
+def test_handle_request_get(mock_file_manager):
+
+    handler = ResponseHandler(file_manager=mock_file_manager)
+
+    request = Request(line=RequestLine(
+        method="GET",
+        url="http://kulawykonfident.com/komendapłock.html",
+        version="HTTP/1.1",
+    ), header=RequestHeader(headers={})
+    )
+
+    expected_response = Response(line=ResponseLine(
+        version="HTTP/1.1",
+        status_code=200,
+    ), header=ResponseHeader(
+        headers={
+            "Content-Type" : "text/html",
+            "Content-Length" : "9",
+            "Last-Modified" : "Fri, 15 May 2026 21:08:42 GMT",
+        }
+    ), body=bytes("pykpykpyk", encoding="utf-8")
+    )
+
+    response = handler.handle_request(request)
+
+    assert response == expected_response
+    
+def test_handle_request_head(mock_file_manager):
+
+    handler = ResponseHandler(file_manager=mock_file_manager)
+
+    request = Request(line=RequestLine(
+        method="HEAD",
+        url="http://kulawykonfident.com/komendapłock",
+        version="HTTP/1.1",
+    ), header=RequestHeader(headers={})
+    )
+
+    expected_response = Response(line=ResponseLine(
+        version="HTTP/1.1",
+        status_code=200,
+    ), header=ResponseHeader(
+        headers={
+            "Content-Type" : "text/html",
+            "Content-Length" : "9",
+            "Last-Modified" : "Fri, 15 May 2026 21:08:42 GMT",
+        }
+    ))
+
+    response = handler.handle_request(request)
+
+    assert response == expected_response
+
+def test_handle_request_put(mock_file_manager):
+
+    handler = ResponseHandler(file_manager=mock_file_manager)
+
+    request = Request(line=RequestLine(
+        method="PUT",
+        url="http://kulawykonfident.com/komendapłock",
+        version="HTTP/1.1",
+    ), header=RequestHeader(headers={}),
+    body=bytes("pykpykpyk", encoding="utf-8")
+    )
+
+    expected_response = Response(line=ResponseLine(
+        version="HTTP/1.1",
+        status_code=201,
+    ), header=ResponseHeader(
+        headers={
+            "Content-Location" : "http://kulawykonfident.com/komendapłock",
+        }
+    ))
+
+    response = handler.handle_request(request)
+
+    assert response == expected_response
+    
+def test_handle_request_options(mock_file_manager):    
+
+    handler = ResponseHandler(file_manager=mock_file_manager)
+
+    request = Request(line=RequestLine(
+        method="OPTIONS",
+        url="http://kulawykonfident.com/komendapłock",
+        version="HTTP/1.1",
+    ), header=RequestHeader(headers={})
+    )
+    
+    expected_response = Response(line=ResponseLine(
+        version="HTTP/1.1",
+        status_code=204,
+    ), header=ResponseHeader(
+        headers={
+            "Allow" : "GET, POST, OPTIONS, HEAD",
+        }
+    ))
+
+    response = handler.handle_request(request=request)
+
+    assert response == expected_response
+
+
+
+
