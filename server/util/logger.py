1b86129decbea3d2730d4a6e8196db7731f4b2a8
twardy7991 <patryktwardowski79@gmail.com>
2026-06-01 13:15:23 +0200

    addded creration of logging folder in case its missing

--- server/util/logger.py
+++ server/util/logger.py
@@ -1,6 +1,7 @@
 import logging
 from server import config
 from datetime import datetime
+import pathlib
 
 config.set_config()
 
@@ -8,8 +9,10 @@ file_name = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 logger : logging.Logger = logging.getLogger(__name__)
 
-print(config.CONFIG)
 if config.CONFIG:
+    if not pathlib.Path(config.CONFIG.logfile).exists():
+        pathlib.Path(config.CONFIG.logfile).mkdir()
+    
     logging.basicConfig(
         filename=config.CONFIG.logfile + f"/{file_name}.log",
         encoding="utf-8",
